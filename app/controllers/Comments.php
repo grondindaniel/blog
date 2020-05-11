588cca7adda16c066b5873dc5ddfbbf83cce9ced
grondindaniel <daniel.grondin@ac-nantes.fr>
2020-05-11 20:32:49 +0200

    add comments controller

--- app/controllers/Comments.php
+++ app/controllers/Comments.php
@@ -1,1 +1,11 @@
 <?php
+
+class Comments extends Acme\Controller
+{
+    public function index($id)
+    {
+        $id = $id[2];
+        $twig = parent::twig();
+        echo $twig->render('comment\index.twig',array('id'=>$id));
+    }
+}
