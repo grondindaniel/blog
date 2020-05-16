68c02e73a3283a4fb18c639a6f73f95e3acaa878
grondindaniel <daniel.grondin@ac-nantes.fr>
2020-05-16 23:51:20 +0200

    add alert message for success

--- app/controllers/Comments.php
+++ app/controllers/Comments.php
@@ -13,8 +13,9 @@ class Comments extends Acme\Controller
         $user_id = $_SESSION['id'];
         $post_id = $_POST['post_id'];
         $role = $_SESSION['role'];
-
         parent::model('Comment')->add($comment_date, $comment, $user_id, $post_id, $role);
+        $twig = parent::twig();
+        echo $twig->render('post\index.twig',array('role'=>$_SESSION['role'],'active'=>$_SESSION['active'],'commentOk'=>'ok'));
     }
 }
 
