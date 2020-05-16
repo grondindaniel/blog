037ff166a70b3ed603b6d80b47ca0caa70362b70
grondindaniel <daniel.grondin@ac-nantes.fr>
2020-05-17 00:06:14 +0200

    indentation of code

--- app/config/Controller.php
+++ app/config/Controller.php
@@ -1,39 +1,39 @@
 <?php
 
-/***
- *
- * this is the main Controller
- *
- */
-
-namespace Acme;
-
-use Twig\Extra\String\StringExtension;
-
-
-class Controller
-{
-    /*
-     * recover data from database
+    /***
+     *
+     * this is the main Controller
+     *
      */
-    public function model($model)
-    {
-        require_once 'app/models/'.$model.'.php';
-        return new $model();
-    }
+
+    namespace Acme;
+
+    use Twig\Extra\String\StringExtension;
 
 
-    /*
-     * Use Twig template for generate views
-     */
-    public function twig()
+    class Controller
     {
-        $loader = new \Twig\Loader\FilesystemLoader('app/views/templates');
-        $loader->addPath('public/dist/css/','css');
-        $loader->addPath('vendor/', 'vendor');
-        $twig = new \Twig\Environment($loader, ['debug' => true]);
-        $twig->addExtension(new StringExtension());
-        $twig->addExtension(new \Twig\Extension\DebugExtension());
-        return $twig;
+        /*
+         * recover data from database
+         */
+        public function model($model)
+        {
+            require_once 'app/models/'.$model.'.php';
+            return new $model();
+        }
+
+
+        /*
+         * Use Twig template for generate views
+         */
+        public function twig()
+        {
+            $loader = new \Twig\Loader\FilesystemLoader('app/views/templates');
+            $loader->addPath('public/dist/css/','css');
+            $loader->addPath('vendor/', 'vendor');
+            $twig = new \Twig\Environment($loader, ['debug' => true]);
+            $twig->addExtension(new StringExtension());
+            $twig->addExtension(new \Twig\Extension\DebugExtension());
+            return $twig;
+        }
     }
-}
