15be8f75a80b604418237c6dc2ebfdeab24dc482
grondindaniel <daniel.grondin@ac-nantes.fr>
2020-05-18 14:17:17 +0200

    controller for form contact

--- app/controllers/Mails.php
+++ app/controllers/Mails.php
@@ -9,14 +9,13 @@ class Mails extends \Acme\Controller
         $phone = strip_tags(htmlspecialchars($_POST['phone']));
         $message = strip_tags(htmlspecialchars($_POST['message']));
 
-//
         $subject = "Website Contact Form:  $name";
         $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-        $header = "From: blog.proteiforme.fr\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+        $header = "From: blog.proteiforme.fr\n";
         $header .= "Reply-To: $email";
         mail("lesbleachos@gmail.com",$subject,$body,$header);
         $twig = parent::twig();
-        echo $twig->render('mail\mail.twig');
+        echo $twig->render('mail\mail.twig',array('contact'=>'ok'));
 
     }
 }
